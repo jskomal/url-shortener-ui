bd3976f0c78a740c683f92aa5c7f25ade2a77536
Jordan Skomal <jskomal@gmail.com>
2022-04-26 10:18:19 -0600

    Add tests for the form

--- cypress/integration/tests.js
+++ cypress/integration/tests.js
@@ -1,11 +1,24 @@
 describe('Url shortener tests', () => {
   beforeEach(() => {
     cy.intercept('GET', 'http://localhost:3001/api/v1/urls', {
-      statusCode: '200',
+      statusCode: 200,
       fixture: 'urls'
     })
   })
-  it('should load the page', () => {
+  it('should load the page and display the stubbed URLs', () => {
     cy.visit('http://localhost:3000/')
+      .get('#2')
+      .contains('Youtube')
+      .get('#3')
+      .contains('Pocket')
+  })
+
+  it('should render the form with the proper inputs', () => {
+    cy.get('input[name="title"]')
+      .invoke('attr', 'placeholder')
+      .should('contain', 'Title...')
+      .get('input[name="urlToShorten"]')
+      .invoke('attr', 'placeholder')
+      .should('contain', 'URL to Shorten...')
   })
 })

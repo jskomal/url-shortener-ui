9cabbdd9177fd5604e1ea1b681cfbb2fb99e9ee5
Jordan Skomal <jskomal@gmail.com>
2022-04-26 10:26:45 -0600

    Add test for POST Req

--- cypress/integration/tests.js
+++ cypress/integration/tests.js
@@ -3,6 +3,14 @@ describe('Url shortener tests', () => {
     cy.intercept('GET', 'http://localhost:3001/api/v1/urls', {
       statusCode: 200,
       fixture: 'urls'
+    }).intercept('POST', 'http://localhost:3001/api/v1/urls', {
+      statusCode: 201,
+      body: {
+        id: 4,
+        long_url: 'www.pizza.example.com',
+        short_url: 'http://localhost:3001/useshorturl/4',
+        title: 'Pizza'
+      }
     })
   })
   it('should load the page and display the stubbed URLs', () => {
@@ -20,5 +28,16 @@ describe('Url shortener tests', () => {
       .get('input[name="urlToShorten"]')
       .invoke('attr', 'placeholder')
       .should('contain', 'URL to Shorten...')
+  })
+
+  it('should be able to type in the forms', () => {
+    cy.get('input[name="title"]').type('Pizza').should('have.value', 'Pizza')
+    cy.get('input[name="urlToShorten"]')
+      .type('www.pizza.example.com')
+      .should('have.value', 'www.pizza.example.com')
+  })
+
+  it('should be able to submit the POST request and have the DOM be updated', () => {
+    cy.get('#formSubmit').click().get('#4').contains('Pizza')
   })
 })

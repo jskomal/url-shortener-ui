7d9e8841e71f9a15035ace1e4946ae367219913f
Jordan Skomal <jskomal@gmail.com>
2022-04-26 10:34:02 -0600

    Add error messaging for App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,17 +6,21 @@ import UrlForm from '../UrlForm/UrlForm'
 
 export const App = () => {
   const [urls, setUrls] = useState([])
+  const [errorMsg, setErrorMsg] = useState('')
 
   useEffect(() => {
-    getUrls().then((data) => {
-      setUrls(data.urls)
-    })
+    getUrls()
+      .then((data) => {
+        setUrls(data.urls)
+      })
+      .catch((err) => setErrorMsg(err))
   }, [])
 
   return (
     <main className='App'>
       <header>
         <h1>URL Shortener</h1>
+        <h2>{errorMsg}</h2>
         <UrlForm setUrls={setUrls} />
       </header>
 

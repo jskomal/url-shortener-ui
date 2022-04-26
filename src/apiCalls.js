665579574721144af59d70d1bf497a72714fcb17
Jordan Skomal <jskomal@gmail.com>
2022-04-26 09:38:30 -0600

    Add POST api call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,13 @@
 export const getUrls = () => {
-  return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
+  return fetch('http://localhost:3001/api/v1/urls').then((response) => response.json())
+}
+
+export const postUrls = (message) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    body: JSON.stringify(message),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then((res) => res.json())
 }
